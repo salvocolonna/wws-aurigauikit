26ee198b4ce45213579b13e50ab661bd0c5a2092
Federico Moretti <federico.moretti@aurigaspa.com>
2019-04-09 15:16:08 +0200

    feat: add version with span instead of div and pre

--- src/main/aurigauikit/components/FormattedCurrencies/FormattedCurrencies.jsx
+++ src/main/aurigauikit/components/FormattedCurrencies/FormattedCurrencies.jsx
@@ -1,26 +1,47 @@
-import React from "react"
-import { FormattedNumber } from "react-intl"
+import React from 'react'
+import { FormattedNumber } from 'react-intl'
 
-function FormattedCurrency({ values, mainCurrency = "EUR", separator = ", ", noZero }) {
+function FormattedCurrency({
+  values,
+  mainCurrency = 'EUR',
+  separator = ', ',
+  noZero,
+  cleanMarkup = false,
+}) {
   if (!values) return null
-  if (typeof values.length === "undefined") values = [values]
+  if (typeof values.length === 'undefined') values = [values]
   if (noZero) values = values.filter(x => x.value !== 0)
   const maybe = v => (v.length !== 0 ? v : [{ currencyCode: mainCurrency, value: 0 }])
-  return (
+  return !cleanMarkup ? (
     <div>
       {maybe(values).map((element, index) => (
         <pre
           key={index}
           style={{
-            fontFamily: "inherit",
-            fontSize: "inherit",
-            margin: "inherit"
+            fontFamily: 'inherit',
+            fontSize: 'inherit',
+            margin: 'inherit',
           }}>
           <FormattedNumber value={element.value} style="currency" currency={element.currencyCode} />
           {values.length > index + 1 && separator}
         </pre>
       ))}
     </div>
+  ) : (
+    <span>
+      {maybe(values).map((element, index) => (
+        <span
+          key={index}
+          style={{
+            fontFamily: 'inherit',
+            fontSize: 'inherit',
+            margin: 'inherit',
+          }}>
+          <FormattedNumber value={element.value} style="currency" currency={element.currencyCode} />
+          {values.length > index + 1 && separator}
+        </span>
+      ))}
+    </span>
   )
 }
 
