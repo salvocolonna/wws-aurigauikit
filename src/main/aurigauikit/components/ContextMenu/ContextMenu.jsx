a93ec5ef55928ae610ac1c5581080e24a7534e3c
Federico Moretti <federico.moretti@aurigaspa.com>
2019-05-28 08:26:01 +0200

    feat: get context also in title

--- src/main/aurigauikit/components/ContextMenu/ContextMenu.jsx
+++ src/main/aurigauikit/components/ContextMenu/ContextMenu.jsx
@@ -1,8 +1,8 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./menu.less"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './menu.less'
 
-import sizeMe from "react-sizeme"
+import sizeMe from 'react-sizeme'
 
 class ContextMenu extends React.Component {
   constructor(props) {
@@ -15,15 +15,15 @@ class ContextMenu extends React.Component {
   static defaultProps = { items: [] }
 
   componentDidMount() {
-    document.body.addEventListener("click", this.closeOnBodyClick)
-    window.addEventListener("resize", this.closeOnResize)
-    document.getElementById("content-dynamic").addEventListener("scroll", this.closeOnResize)
+    document.body.addEventListener('click', this.closeOnBodyClick)
+    window.addEventListener('resize', this.closeOnResize)
+    document.getElementById('content-dynamic').addEventListener('scroll', this.closeOnResize)
   }
 
   componentWillUnmount() {
-    document.body.removeEventListener("click", this.closeOnBodyClick)
-    window.removeEventListener("resize", this.closeOnResize)
-    document.getElementById("content-dynamic").removeEventListener("scroll", this.closeOnResize)
+    document.body.removeEventListener('click', this.closeOnBodyClick)
+    window.removeEventListener('resize', this.closeOnResize)
+    document.getElementById('content-dynamic').removeEventListener('scroll', this.closeOnResize)
   }
 
   closeOnBodyClick(e) {
@@ -40,6 +40,12 @@ class ContextMenu extends React.Component {
     this.props.onCloseRequested()
   }
 
+  title(title) {
+    if (title && typeof title === 'function') {
+      return title(this.props.context)
+    } else return title
+  }
+
   render() {
     const { context, items, size, center } = this.props
     const { position } = context
@@ -50,7 +56,7 @@ class ContextMenu extends React.Component {
       left = center ? position.left - size.width / 2 : position.left - size.width
       top =
         (center ? position.top - size.height / 2 : position.top) -
-        document.getElementById("content-dynamic").scrollTop
+        document.getElementById('content-dynamic').scrollTop
     }
     this.children = []
     return ReactDOM.createPortal(
@@ -58,27 +64,28 @@ class ContextMenu extends React.Component {
         <div
           className="menu"
           id={this.id}
-          style={{ display: "block", top: `${top}px`, left: `${left}px` }}>
+          style={{ display: 'block', top: `${top}px`, left: `${left}px` }}
+        >
           <ul ref={list => (this.list = list)}>
-            {items.map(
-              (item, index) =>
-                item.title ? (
-                  <li
-                    ref={child => this.children.push(child)}
-                    key={index}
-                    className={
-                      item.hidden && item.hidden(context) ? "hidden" : item.style ? item.style : ""
-                    }
-                    onClick={() => this.onItemClick(item)}>
-                    <i
-                      style={{ marginLeft: item.iconName ? "5px" : "" }}
-                      className={item.iconName ? "fa fa-" + item.iconName : "fa fa-fw"}
-                    />
-                    {item.title}
-                  </li>
-                ) : (
-                  <li key={index} style={{ margin: 0 }} className="separator" />
-                )
+            {items.map((item, index) =>
+              item.title ? (
+                <li
+                  ref={child => this.children.push(child)}
+                  key={index}
+                  className={
+                    item.hidden && item.hidden(context) ? 'hidden' : item.style ? item.style : ''
+                  }
+                  onClick={() => this.onItemClick(item)}
+                >
+                  <i
+                    style={{ marginLeft: item.iconName ? '5px' : '' }}
+                    className={item.iconName ? 'fa fa-' + item.iconName : 'fa fa-fw'}
+                  />
+                  {this.title(item.title)}
+                </li>
+              ) : (
+                <li key={index} style={{ margin: 0 }} className="separator" />
+              )
             )}
           </ul>
         </div>
@@ -90,5 +97,5 @@ class ContextMenu extends React.Component {
 
 export default sizeMe({
   monitorHeight: true,
-  monitorPosition: true
+  monitorPosition: true,
 })(ContextMenu)
