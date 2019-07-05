773008b7c1e24346371a2bd0e88d70caa9a21a1e
Pier Roberto <pier.lucisano@aurigaspa.com>
2019-07-05 11:11:13 +0200

    feat: split script in two funcs: publish, update

--- deploy.js
+++ deploy.js
@@ -1,45 +1,45 @@
-/*
-  REQUIREMENTS
-  - Any change in the projects needs to be either committed or discarded before running the script.
-  - A configuration file (name it deploy-config.js) with a variable which is an object as follows:   
-      const wwsDir = { cwd: wwsPath }
-    where wwsPath is the path of the project which you want to update after the new version of the aurigauikit is deployed.
-    wwsDir needs to be exported as follows:
-      module.exports = wwsDir
-
-  WHEN TO RUN THE SCRIPT
-  - Run the script when the update of the version in the AurigaUIKit (in package.json) has been merged into master.
-
-  HOW TO RUN THE SCRIPT
-  - Run node deploy.js in order to run the script.
-
-*/
-
-const wwsDir = require('./deploy-config')
 const { execSync } = require('child_process')
+const fs = require('fs')
 const aurigaDist = { cwd: `./dist` }
+const CONFIG_PATH = './deploy-config.js'
+const option = process.argv.slice(2)[0]
 
 const logExec = (...arg) => {
   console.log(...arg)
   return execSync(...arg)
 }
 
-logExec('git checkout master')
-logExec('git pull')
-try {
-  logExec('yarn unlink aurigauikit', wwsDir)
-} catch (e) {
-  logExec('yarn link', aurigaDist)
-  logExec('yarn link aurigauikit', wwsDir)
-}
-logExec('yarn unlink')
-logExec('yarn build')
-logExec('npm publish', aurigaDist)
-logExec('yarn link', aurigaDist)
-logExec('git checkout master', wwsDir)
-logExec('git pull', wwsDir)
-logExec('yarn add aurigauikit', wwsDir)
-logExec('yarn link aurigauikit', wwsDir)
-logExec('git status', wwsDir)
+if (option === '--publish') {
+  publish()
+} else if (option === '--update') {
+  update()
+} else {
+  console.warn('Add a flag:')
+  console.warn('  --publish in order to publish a new AurigaUIKit version')
+  console.warn('  --update in order to update your local project')
+  console.warn('More info in deploy.md')
 
-console.log('There should be package.json and package.lock ready to be staged')
+  process.exit()
+}
+
+function publish() {
+  logExec('git checkout master')
+  logExec('git pull')
+  logExec('yarn unlink')
+  logExec('yarn build')
+  logExec('npm publish', aurigaDist)
+  logExec('yarn link', aurigaDist)
+}
+
+function update() {
+  try {
+    fs.readFileSync(CONFIG_PATH)
+  } catch (e) {
+    throw 'This error probably occured due to a missing configuration file. Read documentation in deploy.md for more details. More info below: \n' +
+      e.message
+  }
+  const wwsDir = require(CONFIG_PATH)
+  logExec('yarn add aurigauikit', wwsDir)
+  logExec('yarn link aurigauikit', wwsDir)
+  logExec('git status', wwsDir)
+}
