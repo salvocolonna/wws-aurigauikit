4a6ac57a8986326d91b56f939d11abecf752b2df
Federico Moretti <federico.moretti@aurigaspa.com>
2019-07-23 15:42:40 +0200

    feat: changes to use auth without form

--- src/main/aurigauikit/components/LoginPage/LoginPage.jsx
+++ src/main/aurigauikit/components/LoginPage/LoginPage.jsx
@@ -1,11 +1,56 @@
 import React from 'react'
 import { TOKEN_STORAGE_KEY } from 'aurigauikit/constants'
 import temporaryPanels from '../temporary-panels'
+import Spinner from '../Spinner'
+
 import './style.css'
 
 /* 
 logo: {src: image, alt: string}
 */
+
+function buildAuth({ authParams, tokenPath, type, username, password }) {
+  const queryParams = Object.entries(authParams).reduce((string, [key, value]) => {
+    return string + key + '=' + value + '&'
+  }, '?')
+
+  let authUrl = tokenPath + queryParams
+
+  const authOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  }
+
+  switch (type) {
+    case 'basic':
+      authOptions.headers['X-Authorization'] = btoa(`${username}:${password}`)
+      break
+    case 'query':
+      authUrl = authUrl + `username=${username}&password=${password}`
+      break
+    default:
+      break
+  }
+  return [authUrl, authOptions]
+}
+
+async function authenticateAndGetUser(authUrl, authOptions, userProfilePath) {
+  const authResponse = await fetch(authUrl, authOptions)
+  const authData = await authResponse.json()
+  localStorage.setItem(TOKEN_STORAGE_KEY, JSON.stringify(authData))
+
+  const userResponse = await fetch(userProfilePath, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${authData.access_token}`,
+    },
+  })
+
+  if (userResponse.status !== 200) throw new Error(userResponse.statusText)
+  const userData = await userResponse.json()
+  return userData
+}
+
 function LoginPage(props) {
   const {
     logo,
@@ -15,52 +60,69 @@ function LoginPage(props) {
     type = 'basic',
     onLogin = data => console.log('login successful', data),
     onError,
+    messages = {
+      error: 'An error occurred, retry later!',
+      invalid: 'Invalid username or password!',
+    },
   } = props
 
   const [username, setUsername] = React.useState('')
   const [password, setPassword] = React.useState('')
   const [loading, setLoading] = React.useState(false)
 
+  React.useEffect(() => {
+    async function login() {
+      if (authParams.grant_type === 'client_credentials') {
+        setLoading(true)
+
+        const [authUrl, authOptions] = buildAuth({
+          authParams,
+          tokenPath,
+          type,
+        })
+
+        try {
+          const userData = await authenticateAndGetUser(authUrl, authOptions, userProfilePath)
+          onLogin(userData)
+          setLoading(false)
+        } catch (error) {
+          setLoading(false)
+          if (onError && typeof onError === 'function') {
+            onError(error)
+          } else {
+            console.log(error.message)
+            temporaryPanels.showCriticalTemporaryPanel(messages.error)
+          }
+        }
+      }
+    }
+
+    login()
+  }, [
+    authParams,
+    authParams.grant_type,
+    tokenPath,
+    type,
+    userProfilePath,
+    onLogin,
+    onError,
+    messages.error,
+  ])
+
   async function submit(event) {
-    event.preventDefault()
-    const queryParams = Object.entries(authParams).reduce((string, [key, value]) => {
-      return string + key + '=' + value + '&'
-    }, '?')
+    if (event) event.preventDefault()
 
-    let authUrl = tokenPath + queryParams
-
-    const authOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    }
-
-    switch (type) {
-      case 'basic':
-        authOptions.headers['X-Authorization'] = btoa(`${username}:${password}`)
-        break
-      case 'query':
-        authUrl = authUrl + `username=${username}&password=${password}`
-        break
-      default:
-        break
-    }
+    const [authUrl, authOptions] = buildAuth({
+      authParams,
+      tokenPath,
+      type,
+      password,
+      username,
+    })
 
     try {
       setLoading(true)
-      const authResponse = await fetch(authUrl, authOptions)
-      const authData = await authResponse.json()
-      localStorage.setItem(TOKEN_STORAGE_KEY, JSON.stringify(authData))
-
-      const userResponse = await fetch(userProfilePath, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${authData.access_token}`,
-        },
-      })
-
-      if (userResponse.status !== 200) throw new Error(userResponse.statusText)
-      const userData = await userResponse.json()
-
+      const userData = await authenticateAndGetUser(authUrl, authOptions, userProfilePath)
       setLoading(false)
       onLogin(userData)
     } catch (error) {
@@ -69,7 +131,7 @@ function LoginPage(props) {
         onError(error)
       } else {
         console.log(error.message)
-        temporaryPanels.showCriticalTemporaryPanel('Invalid username or password!')
+        temporaryPanels.showCriticalTemporaryPanel(messages.invalid)
       }
     }
   }
@@ -86,42 +148,49 @@ function LoginPage(props) {
                 <img src={logo.src} alt={logo.alt} />
               </div>
             )}
-            <div id="login-main">
-              <form method="POST">
-                <div style={{ paddingLeft: 40, paddingRight: 40 }}>
-                  <label>
-                    Username
-                    <input
-                      value={username}
-                      style={{ width: '100%', marginTop: 3 }}
-                      onChange={e => setUsername(e.target.value)}
-                      type="text"
-                    />
-                  </label>
-                </div>
-                <div style={{ paddingLeft: 40, paddingRight: 40 }}>
-                  <label style={{ marginTop: 20 }}>
-                    Password
-                    <input
-                      value={password}
-                      style={{ width: '100%', marginTop: 3 }}
-                      onChange={e => setPassword(e.target.value)}
-                      type="password"
-                    />
-                  </label>
-                </div>
-                <div style={{ padding: '20px 40px' }}>
-                  <button
-                    disabled={isDisabled || loading}
-                    onClick={submit}
-                    style={{ width: '50%', float: 'right' }}
-                    className="btn btn-primary"
-                  >
-                    Login
-                  </button>
-                </div>
-              </form>
-            </div>
+            {loading && authParams.grant_type === 'client_credentials' && (
+              <div id="login-spinner" style={{ position: 'relative', height: 100, width: '100%' }}>
+                <Spinner target={'login-spinner'} style={{ backgroundColor: 'transparent' }} />
+              </div>
+            )}
+            {authParams.grant_type === 'password' && (
+              <div id="login-main">
+                <form method="POST">
+                  <div style={{ paddingLeft: 40, paddingRight: 40 }}>
+                    <label>
+                      Username
+                      <input
+                        value={username}
+                        style={{ width: '100%', marginTop: 3 }}
+                        onChange={e => setUsername(e.target.value)}
+                        type="text"
+                      />
+                    </label>
+                  </div>
+                  <div style={{ paddingLeft: 40, paddingRight: 40 }}>
+                    <label style={{ marginTop: 20 }}>
+                      Password
+                      <input
+                        value={password}
+                        style={{ width: '100%', marginTop: 3 }}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
+                      />
+                    </label>
+                  </div>
+                  <div style={{ padding: '20px 40px' }}>
+                    <button
+                      disabled={isDisabled || loading}
+                      onClick={submit}
+                      style={{ width: '50%', float: 'right' }}
+                      className="btn btn-primary"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </form>
+              </div>
+            )}
           </div>
         </div>
       </div>
