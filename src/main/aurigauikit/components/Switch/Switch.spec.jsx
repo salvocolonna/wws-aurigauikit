950e7c3825b77ad24db2e072bdfe7eae00443c53
roland <roland.peraj@gmail.com>
2019-08-05 12:38:53 +0200

    fix: tests

--- src/main/aurigauikit/components/Switch/Switch.spec.jsx
+++ src/main/aurigauikit/components/Switch/Switch.spec.jsx
@@ -1,19 +1,19 @@
-import React from "react"
-import renderer from "react-test-renderer"
-import { shallow } from "enzyme"
-import Switch from "./Switch"
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import Switch from './Switch'
 
-describe("Switch", () => {
-  it("displays as expected", () => {
+describe('Switch', () => {
+  it('displays as expected', () => {
     const component = renderer.create(<Switch checked />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
 
-  it("calls onChange callback", () => {
+  it('calls onChange callback', () => {
     const onChange = jest.fn()
     const component = shallow(<Switch onChange={onChange} />)
-    component.find("input").simulate("change")
+    component.find('input').simulate('change')
     expect(onChange.mock.calls).toHaveLength(1)
   })
 })
