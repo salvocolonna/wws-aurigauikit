74b41ec0f41d88bfea4cebd0e1abc73c1768686e
Federico Moretti <Federico.Moretti@aurigaspa.com>
2019-09-13 15:49:11 +0200

    Fix css rule `div.static-panel i` that breaks antd select icon
    Changelog:
    - changed `div.static-panel i` to `div.static-panel > i`

--- src/main/aurigauikit/components/StaticPanel.jsx
+++ src/main/aurigauikit/components/StaticPanel.jsx
@@ -1,26 +1,27 @@
-import React from "react"
+import React from 'react'
 
 const icons = {
-  info: "info",
-  confirmatory: "check",
-  critical: "times",
-  warning: "exclamation"
+  info: 'info',
+  confirmatory: 'check',
+  critical: 'times',
+  warning: 'exclamation',
 }
 
 export default function StaticPanel(props) {
-  const { style, type = "info" } = props
+  const { style, type = 'info' } = props
   const children = React.Children.toArray(props.children)
   const content = children.find(({ type }) => type !== StaticPanel.Icon)
   let icon = children.find(({ type }) => type === StaticPanel.Icon)
-  const flexClassName = icon ? "-flex" : ""
+  const flexClassName = icon ? '-flex' : ''
 
-  if (icon) icon = React.cloneElement(icon, { type })
+  if (icon) icon = React.cloneElement(icon, { type, className: 'static-panel-left-icon' })
   return (
     <div
       className={`static-panel${flexClassName && flexClassName} static-panel-${type}`}
-      style={{ margin: "0px", ...style }}>
+      style={{ margin: '0px', ...style }}
+    >
       {icon ? (
-        <div style={{ display: "flex", flexBasis: "100%", marginRight: "1em" }}>
+        <div style={{ display: 'flex', flexBasis: '100%', marginRight: '1em' }}>
           {icon} {content}
         </div>
       ) : (
--- src/main/demo/components/StaticPanel/Base.jsx
+++ src/main/demo/components/StaticPanel/Base.jsx
@@ -1,10 +1,11 @@
-import React from "react"
-import StaticPanel from "aurigauikit/components/StaticPanel"
+import React from 'react'
+import StaticPanel from 'aurigauikit/components/StaticPanel'
+import { Select } from 'antd'
 
 export default class extends React.Component {
   render() {
     return (
-      <div style={{ width: "100%" }}>
+      <div style={{ width: '100%' }}>
         <StaticPanel style={{ marginBottom: 20 }}>
           <StaticPanel.Icon />
           <div>This is an info type panel with icon.</div>
@@ -17,9 +18,18 @@ export default class extends React.Component {
           <StaticPanel.Icon />
           <div>This is a critical type panel with icon.</div>
         </StaticPanel>
-        <StaticPanel type="warning">
+        <StaticPanel type="warning" style={{ marginBottom: 20 }}>
           <StaticPanel.Icon />
           <div>This is a warning type panel with icon.</div>
+        </StaticPanel>
+        <StaticPanel type="warning" style={{ marginBottom: 20 }}>
+          <StaticPanel.Icon />
+          <div>
+            <Select defaultValue="pier_roberto">
+              <Select.Option value="pier_roberto">Pier Roberto</Select.Option>
+              <Select.Option value="roland">Roland</Select.Option>
+            </Select>
+          </div>
         </StaticPanel>
       </div>
     )
