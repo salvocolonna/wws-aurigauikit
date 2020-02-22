352762e527bcf9f340b4bddf5c86c9def3e82682
roland <roland.peraj@aurigaspa.com>
2020-02-22 11:02:24 +0100

    add antd radio

---
+++ src/main/aurigauikit/antd.js
@@ -0,0 +1,30 @@
+// Add here the components you need
+import {
+  Row,
+  Col,
+  Select,
+  Form,
+  Checkbox,
+  Cascader,
+  Input,
+  Collapse,
+  Tooltip,
+  Switch,
+  Slider,
+  Radio,
+} from 'antd'
+export default {
+  Row,
+  Col,
+  Select,
+  Form,
+  Checkbox,
+  Cascader,
+  Input,
+  Collapse,
+  Tooltip,
+  Switch,
+  Slider,
+  Radio,
+}
+export * from 'antd'
