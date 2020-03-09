e0dddfb93ce15619af6cdd8bd4b7bd995b4299fa
Enzo Liccese <enzol@aurigaactive.aurigainformatica.it>
2020-03-09 12:18:50 +0100

    Replaced Radio with antd wrapper

--- src/main/aurigauikit/components/Radio/Radio.jsx
+++ src/main/aurigauikit/components/Radio/Radio.jsx
@@ -1,35 +1,8 @@
 import React from "react"
-import "./radio.less"
+import {Radio as AntRadio} from "antd"
 
-class Radio extends React.Component {
-  constructor(props) {
-    super(props)
-    this.id = guid()
-  }
-
-  render() {
-    const { isChecked, isDisabled, children, onChange, style } = this.props
-    const divStyle = {
-      cursor: isDisabled ? "default" : "pointer",
-      pointerEvents: isDisabled ? "none" : "all",
-      ...style
-    }
-    return (
-      <div style={divStyle} onClick={() => onChange(!isChecked)}>
-        <input
-          type="radio"
-          id={this.id}
-          style={{ pointerEvents: "none" }}
-          onChange={e => e.preventDefaults()}
-          checked={isChecked}
-          disabled={isDisabled}
-        />
-        <label htmlFor={this.id} style={{ pointerEvents: "none" }}>
-          {children}
-        </label>
-      </div>
-    )
-  }
+const Radio = ({ isChecked, isDisabled, children, onChange,...props }) =>{
+return <AntRadio checked={isChecked} disabled={isDisabled} onChange={onChange} {...props} />
 }
 
 export default Radio
