d1c24b304d31a205fa3702e36d25081ae9312dcf
Enzo Liccese <enzol@aurigaactive.aurigainformatica.it>
2020-03-09 12:33:05 +0100

    remove useless props

--- src/main/aurigauikit/components/Radio/Radio.jsx
+++ src/main/aurigauikit/components/Radio/Radio.jsx
@@ -1,8 +1,8 @@
-import React from "react"
-import {Radio as AntRadio} from "antd"
+import React from 'react'
+import { Radio as AntRadio } from 'antd'
 
-const Radio = ({ isChecked, isDisabled, children, onChange,...props }) =>{
-return <AntRadio checked={isChecked} disabled={isDisabled} onChange={onChange} {...props} />
+const Radio = ({ isChecked, isDisabled, ...props }) => {
+  return <AntRadio checked={isChecked} disabled={isDisabled} {...props} />
 }
 
 export default Radio
